8d1a44b77ceb6ee7e92ee40fa4716c253cca0067
yui <ycui@microstrategy.com>
2017-07-04 13:50:53 +0800

    update

--- tech_index/macd.py
+++ tech_index/macd.py
@@ -18,22 +18,44 @@ class MACD(object):
             full_path = os.path.join(self.hist_day_path, file_name)
             self.df = DataFrame.from_csv(full_path)
             self.close = self.df.close.values
+            self.volume = self.df.volume.values
             self.dif, self.dea, self.macd = talib.MACD(self.close, fastperiod=12, slowperiod=26, signalperiod=9)
             return self.dif, self.dea, self.macd, self.df.date.values
         except Exception, e:
             print "getMACD error %s" %str(e)
     
+    def isUpCross(self):
+        ret = False
+        if self.dif[-1] >= self.dea[-1]:
+            if self.dif[-2] < self.dea[-2] and self.dif[-3] < self.dea[-3]:
+                return True
+    
+    def isSmallIncPrice(self):
+        ret = False
+        if (self.close[-1] - self.close[-2] > 0) and (self.close[-1] - self.close[-2]) / self.close[-2] <= 0.015:
+            ret = True
+
+        return ret
+
+    def isDeclineVolume(self):
+        ret = False
+        if self.volume[-1] < self.volume[-2]:
+            ret = True
+        
+        return ret
+
     # MUST call after getMACD
     # strategy 0: simple cross
     # strategy 1: cross and small increase price
     # strategy 2: cross; decline volume; small increase price
-    
     def canBuy(self, strategy=2):
         ret = False
-        if self.dif[-1] >= self.dea[-1]:
-            if self.dif[-2] < self.dea[-2] and self.dif[-3] < self.dea[-3]:
-                if (self.close[-1] - self.close[-2]) / self.close[-2] <= 0.015:
-                    ret = True
+        if strategy == 0:
+            ret = self.isUpCross()
+        elif strategy == 1:
+            ret = self.isUpCross() and self.isSmallIncPrice()
+        elif strategy == 2:
+            ret = self.isUpCross() and self.isSmallIncPrice() and self.isDeclineVolume()
             
         return ret
         
@@ -44,6 +66,6 @@ if __name__ == "__main__":
     dif, dea, macd, date_str = m.getMACD()
     #for i in range(len(dif)):
     #    print "%s: DIF: %s, DEA: %s, MACD: %s" %(date_str[i], dif[i], dea[i], 2*macd[i])
-    m.canBuy()
+    m.canBuy(2)
     
     
