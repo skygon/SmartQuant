efae549e50c829e16da29f39b4869ed1de13763c
yui <ycui@microstrategy.com>
2017-07-05 13:38:46 +0800

    update

--- tech_index/kdj.py
+++ tech_index/kdj.py
@@ -1,1 +1,33 @@
-# kdj
+import os
+import talib as ta
+from pandas import DataFrame
+
+
+class KDJ(object):
+    def __init__(self, code):
+        self.code = code
+        self.hist_day_path = os.path.join(os.getcwd(), 'hist_data', 'day')
+    
+    def setCode(self, code):
+        self.code = code
+
+    
+    def getKDJ(self):
+        file_name = self.code + '_hist_d.csv'
+        full_path = os.path.join(self.hist_day_path, file_name)
+
+        df = DataFrame.from_csv(full_path)
+        high = df.high.values
+        low = df.low.values
+        close = df.close.values
+        self.date = df.date.values
+        # matype: 0=SMA, 1=EMA, 2=WMA, 3=DEMA, 4=TEMA, 5=TRIMA, 6=KAMA, 7=MAMA, 8=T3 (Default=SMA)
+        K, D = ta.STOCH(high, low, close, fastk_period=9,slowk_period=3,slowk_matype=1,slowd_period=3,slowd_matype=1)
+        return K, D
+
+if __name__ == "__main__":
+    kdj = KDJ('603993')
+    k, d = kdj.getKDJ()
+    for i in range(len(k)):
+        print kdj.date[i], k[i], d[i]
+    
---
+++ tech_index/rsi.py
@@ -0,0 +1,31 @@
+import os
+import talib as ta
+from pandas import DataFrame
+
+class RSI(object):
+    def __init__(self, code):
+        self.code = code
+        self.hist_day_path = os.path.join(os.getcwd(), 'hist_data', 'day')
+
+    def setCode(self, code):
+        self.code = code
+
+    def getRSI(self):
+        file_name = self.code + '_hist_d.csv'
+        full_path = os.path.join(self.hist_day_path, file_name)
+        self.df = DataFrame.from_csv(full_path)
+        close = self.df.close.valuse
+        rsi = {}
+        rsi['6'] = ta.RSI(close, timeperiod=6)
+        rsi['12'] = ta.RSI(close, timeperiod=12)
+        rsi['24'] = ta.RSI(close, timeperiod=24)
+        return rsi
+    
+
+if __name__ == "__main__":
+    r = RSI('603993')
+    rsi = r.getRSI()
+    date = r.df.date.values
+
+    for i in range(len(date)):
+        print date[i], rsi['6'][i], rsi['12'][i], rsi['24'][i]
