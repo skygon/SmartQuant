9642be0dac38062ddac008e359f50e661fa2c192
yui <ycui@microstrategy.com>
2017-07-19 14:02:18 +0800

    update

--- tech_index/TickPrice.py
+++ tech_index/TickPrice.py
@@ -16,9 +16,21 @@ class TickPrice(VolumeBase):
         # override base class
         self.current_date = date_str
         self.tick_data_path = os.path.join(os.getcwd(), 'tick_data')
+        self.up = {}
+        self.down = {}
+        # for strategy statistic
         self.total = 0
         self.stock_pool = 0
+        self.initStatisticData()
 
+    def initStatisticData(self):
+        self.up['count'] = 0
+        self.up['price'] = 0
+        self.up['volume'] = 0
+
+        self.down['count'] = 0
+        self.down['price'] = 0
+        self.down['volume'] = 0
 
     # override base class. And before invoke, must setCode
     def prepareData(self):
@@ -29,6 +41,33 @@ class TickPrice(VolumeBase):
         f = os.path.join(self.tick_data_path, file_name)
         self.df.to_csv(f)
     
+    def prepareDataFromDisk(self):
+        file_name = self.code + ".csv"
+        f = os.path.join(self.tick_data_path, file_name)
+        self.df = DataFrame.from_csv(f)
+    
+    def getSummary(self):
+        self.date = self.df.date.values
+        self.open = self.df.open.values
+        self.close = self.df.close.values
+        self.high = self.df.high.values
+        self.low = self.df.low.values
+        self.volume = self.df.volume.values
+
+        self.length = len(self.date)
+        for i in range(self.length):
+            if self.open[i] >= self.close[i]:
+                self.down['count'] += 1
+                self.down['price'] += (self.open[i] - self.close[i])
+            else:
+                self.up['count'] += 1
+                self.up['price'] += (self.close[i] - self.open[i])
+
+        print "===== total[%s] / up[%s] / down[%s] =====" %(self.length, self.up['count'], self.down['count'])
+        print "===== Price change: total[%s] / up[%s] / down[%s] =====" %((self.close[self.length-1] - self.open[0]), self.up['price'], self.down['price'])
+
+
+
     def canBuy(self):
         try:
             self.prepareData()
@@ -43,5 +82,5 @@ class TickPrice(VolumeBase):
 if __name__ == "__main__":
     t = TickPrice('2017-07-19')
     t.setCode('603993')
-    t.prepareData()
-    print t.df
+    t.prepareDataFromDisk()
+    t.getSummary()
