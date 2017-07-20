daf9079dc57ff641faa4a2f3bc589186651c8f7f
yui <ycui@microstrategy.com>
2017-07-20 13:31:43 +0800

    update

--- information_service/market_quick_info.py
+++ information_service/market_quick_info.py
@@ -5,3 +5,12 @@ import urllib2
 sys.path.append(os.getcwd())
 from utils import *
 
+# xueqiu
+quick_info_url = "https://xueqiu.com/v4/statuses/public_timeline_by_category.json?since_id=-1&max_id=-1&count=5&category=6"
+
+class QuickInfo(object):
+    def __init__(self):
+        pass
+
+
+    def getFirstPage()
