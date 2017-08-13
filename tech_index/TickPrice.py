908ba39cdb8d9e69384ec64579e62cd93efec48e
skygon <skygoncui@163.com>
2017-08-13 22:43:11 +0800

    update

--- tech_index/TickPrice.py
+++ tech_index/TickPrice.py
@@ -146,7 +146,7 @@ class TickPrice(VolumeBase):
 
 
 def monitor():
-    t = TickPrice('2017-08-02')
+    t = TickPrice('2017-07-31')
     while True:
         try:
             c = g_utils.full_queue.get(False)
